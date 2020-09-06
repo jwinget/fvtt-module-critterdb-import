d3e547ddf3957eccd3be791968e5f44a3d0d1a22
Jason Winget <jwinget@gmail.com>
2020-09-06 19:04:53 -0400

    compendium check/creation

--- scripts/critterdb-import.js
+++ scripts/critterdb-import.js
@@ -1,18 +1,28 @@
 // Parse CritterDB JSON data
 
-// Generate Foundry-formatted JSON
+// Determine if this is a single monster or a bestiary
+
+// Generate Foundry-formatted JSON data
 
 // Check to see if compendium exists already, create if it doesn't
+// Reference a Compendium pack by it's collection ID
+let pack = game.packs.find(p => p.collection === `critterdb-${bestiary}`);
 
-// Below is boilerplate from Foundry Docs on importing actor data
-// Reference a Compendium pack by it's callection ID
-const pack = game.packs.find(p => p.collection === `${moduleName}.${packName}`);
+if (pack == null) {
+    // Create a new compendium
+    await Compendium.create({
+        name: `critterdb-${bestiary}`,
+        label: `CritterDB Imported-${bestiary}`,
+        entity: "Actor",
+      });
+}
 
 // Load an external JSON data file which contains data for import
 // Need to change this to instead use the translated JSON
 const response = await fetch("worlds/myworld/data/import.json");
 const content = await response.json();
 
+// I think the below should work once the JSON payload is correctly formatted
 // Create temporary Actor entities which impose structure on the imported data
 const actors = Actor.createMany(content, {temporary: true});
 
