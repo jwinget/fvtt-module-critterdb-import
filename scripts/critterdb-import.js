d124d757cb701e414443a4349f817f814ca765ee
Jason Winget <jwinget@gmail.com>
2020-09-06 20:12:39 -0400

    done for tonight I think

--- scripts/critterdb-import.js
+++ scripts/critterdb-import.js
@@ -31,39 +31,38 @@ class CritterDBImporter extends Application
 
   async parseCritter() {
     // Set up a default bestiary for single-mob imports
-let bestiary = "MyCritters";
+    let bestiary = "MyCritters";
 
-// Parse CritterDB JSON data
-// Determine if this is a single monster or a bestiary
+    // Parse CritterDB JSON data pasted in UI
+    // Determine if this is a single monster or a bestiary
+    // Within a bestiary all of the critters are in a creatures array
 
-// Generate Foundry-formatted JSON data
+    // Generate Foundry-formatted JSON data
+    // This should be assigned to const content
 
-// Check to see if compendium exists already, create if it doesn't
-// Reference a Compendium pack by it's collection ID
-let pack = game.packs.find(p => p.collection === `critterdb-${bestiary}`);
+    // Check to see if compendium exists already, create if it doesn't
+    // Reference a Compendium pack by it's collection ID
+    let pack = game.packs.find(p => p.collection === `critterdb-${bestiary}`);
 
-if (pack == null) {
-    // Create a new compendium
-    await Compendium.create({
-        name: `critterdb-${bestiary}`,
-        label: `CritterDB - ${bestiary}`,
-        entity: "Actor"
-      });
-}
+    if (pack == null) {
+        // Create a new compendium
+        await Compendium.create({
+            name: `critterdb-${bestiary}`,
+            label: `CritterDB - ${bestiary}`,
+            entity: "Actor"
+          });
+    }
 
-// Load an external JSON data file which contains data for import
-// Need to change this to instead use the translated JSON
-const response = await fetch("worlds/myworld/data/import.json");
-const content = await response.json();
+    // I think the below should work once the JSON payload is correctly formatted
+    // Create temporary Actor entities which impose structure on the imported data
+    // Looks like you have to call individually Actor.create() now. createMany() doesn't work anymore
+    // may also be able to use Actor.importFromJSON() 
+    // const actors = Actor.createMany(content, {temporary: true});
 
-// I think the below should work once the JSON payload is correctly formatted
-// Create temporary Actor entities which impose structure on the imported data
-const actors = Actor.createMany(content, {temporary: true});
-
-// Save each temporary Actor into the Compendium pack
-for ( let a of actors ) {
-  await pack.importEntity(a);
-  console.log(`Imported Actor ${a.name} into Compendium pack ${pack.collection}`);
-}
+    // Save each temporary Actor into the Compendium pack
+    for ( let a of actors ) {
+      await pack.importEntity(a);
+      console.log(`Imported Actor ${a.name} into Compendium pack ${pack.collection}`);
+    }
   }
 }
